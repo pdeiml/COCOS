054f14065c09877b40c5cd8e0e1858f5ecbafe7b
AndiZm <34646647+AndiZm@users.noreply.github.com>
2018-10-02 13:30:49 +0200

    Update COCOShpc.cpp

--- src_exe/COCOShpc.cpp
+++ src_exe/COCOShpc.cpp
@@ -979,6 +979,8 @@ int main (int argc, char* argv[])
       fileout << "# File: " << argv[1] << "\n";
       fileout << "# is " << 1e-12 * vSetting.GetStartEvalTime() << ";\tie " << 1e-12 * vSetting.GetEndEvalTime() << ";\tsl " << vSetting.GetTimeLimitation() << "\n";
       fileout << "# ts " << taubeg + (binwidth/2) << ";\tte " << tauend + (binwidth/2) << ";\tnb " << nbins << " ->Width: " << binwidth << "\n";
+      fileout << "# Real start time, end time and measurement time [s]:\n";
+      fileout << 1e-12 * startmeastime << "\t" << 1e-12 * endmeastime << "\t" << 1e-12 * meastime << "\n";
       fileout << "#---------------------------------------------------\n";
       for (int b=0; b<nbins; b++)
       {
